357517f7eff4e6a0c68a94384fabaf48afbac7bf
Aristia <80077178+Xanlete@users.noreply.github.com>
2022-06-26 17:03:17 -0400

    Update vexing.py

--- vexing/vexing.py
+++ vexing/vexing.py
@@ -6,22 +6,6 @@ Cog = getattr(commands, "Cog", object)
 
 logger = logging.getLogger("Modmail")
 
-
-class Vex(Cog):
-    """
-    Replies with a witty remark. Give it a try if you think you're strong enough to take it.
-    """
-
-    vex = [
-        ""
-    ]
-
-
-    def __init__(self, bot):
-        super().__init__()
-        self.bot = bot
-        # self.db = bot.plugin_db.get_partition(self)
-
     @commands.command(name="vexing", aliases=["vex"])
     async def _aki(self, ctx):
         """
